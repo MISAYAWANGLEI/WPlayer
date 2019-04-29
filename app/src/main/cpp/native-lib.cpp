fe4c3835af7479a6e3eb9fc8d2c416a791dd2708
wanglei <wanglei55@xin.com>
2019-04-29 18:09:47 +0800

    初始化

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,15 @@
+#include <jni.h>
+#include <string>
+
+extern "C"{
+#include "libavformat/avformat.h"
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_wanglei_wplayer_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+
+    return env->NewStringUTF(hello.c_str());
+}
---
+++ app/src/main/java/com/wanglei/wplayer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.wanglei.wplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    public native String stringFromJNI();
+}
